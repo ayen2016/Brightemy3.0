f387f81a60c4989b821628605540fa79a2e92c45
Ayen Uddin <ayen.uddin@voktis.com>
2025-08-17 16:43:08 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+<script>
+
+
+const observer = new IntersectionObserver((entries) => {
+
+  entries.forEach(entry => {
+
+    if (entry.isIntersecting){
+
+      entry.target.classList.add('show');
+    }
+
+  });
+
+});
+
+const target= document.querySelector('.partner_display');
+observer.observe(target);
+
+
+
+</script>
+
+<script>
+
+
+
+const observer = new IntersectionObserver((entries) => {
+
+  entries.forEach(entry => {
+
+    if (entry.isIntersecting){
+
+      entry.target.classList.add('show');
+    }
+
+  });
+
+});
+
+const target= document.querySelector('.skill_area_div2');
+observer.observe(target);
+
+
+
+
+
+</script>
+
+
+
+const observer = new IntersectionObserver((entries) => {
+
+  entries.forEach(entry => {
+
+    if (entry.isIntersecting){
+
+      entry.target.classList.add('show');
+    }
+
+  });
+
+});
+
+const target= document.querySelector('.partner_display');
+observer.observe(target);
+
+
+const target= document.querySelector('.skill_area_div2');
+observer.observe(target);
+
+
+
+
+const observer = new IntersectionObserver((entries) => {
+
+  entries.forEach(entry => {
+
+    if (entry.isIntersecting){
+
+      entry.target.classList.add('show');
+    }
+
+  });
+
+});
+
+const target= document.querySelector('.skill_area_div2');
+observer.observe(target);
